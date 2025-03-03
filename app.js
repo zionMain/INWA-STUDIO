67a831bd4d49a200dbe74868766177e500e68dbd
zionMain <ziongxx12@gmail.com>
2025-03-03 18:41:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
+
+
+
